34bc932f332a3a2e3f6aa0a7cb3348d1a1196679
刘小平 <movedisk@qq.com>
2023-01-18 18:03:27 +0800

    工程代码初始化。

---
+++ src/main/java/alice/code/retro/game/AliceRetroGameApplication.java
@@ -0,0 +1,17 @@
+package alice.code.retro.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Alice RetroGame DB
+ * 怀旧游戏机数据库
+ * created time 2023.1.18
+ * @author contact@liuxp.me
+ */
+@SpringBootApplication
+public class AliceRetroGameApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AliceRetroGameApplication.class, args);
+	}
+}
